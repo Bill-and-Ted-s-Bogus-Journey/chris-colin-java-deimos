f46ffc33edacaea7121f878d14b78e7729af8f74
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-22 10:17:47 -0600

    java bonus question

--- src/chrisjava.java
+++ src/chrisjava.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 
 public class chrisjava {
-    Scanner scanner = new Scanner;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number you would like to see cubed");
+        int userNumber = scanner.nextInt();
+
+        int i = userNumber;
+
+        int cubed = i * i * i;
+        System.out.printf("Your number cubed is: %d", cubed);
+
+
+    }
 }
