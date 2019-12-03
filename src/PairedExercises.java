4c2ee0e66e06f5780de453903805efaf484f04fd
colinmarchbanks <marchbanks.colin@gmail.com>
2019-12-03 15:03:23 -0600

    Merging

--- src/PairedExercises.java
+++ src/PairedExercises.java
@@ -1,13 +1,17 @@
 import java.util.Arrays;
 
 public class PairedExercises {
+    public static void main(String[] args) {
 
-    private String[] ary = tester.split(" ");
-    private int[] ary2 = new int[ary.length];
-        for(int i = 0; i < ary.length ; i++){
+    String tester = new String("1 2 3 4 5 6 7 10 -1");
+
+    String[] ary = tester.split(" ");
+    int[] ary2 = new int[ary.length];
+    for(int i = 0; i < ary.length ; i++){
         ary2[i] = Integer.parseInt(ary[i]);
 
         Arrays.sort(ary2);
         System.out.println(ary2[ary2.length - 1] + " " + ary2[0]);
+    }
     }
 }
