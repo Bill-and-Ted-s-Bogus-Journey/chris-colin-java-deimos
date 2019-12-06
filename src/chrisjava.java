0c78fd3a3e45c20efbc428367ea4e9fc436c08c9
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-05 21:14:33 -0600

    exercise to capitalize first letter of each string and exercise that takes in a string and returns the string in reverse

--- src/chrisjava.java
+++ src/chrisjava.java
@@ -1,6 +1,9 @@
+
 import java.util.Scanner;
 
 public class chrisjava {
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,17 +16,8 @@ public class chrisjava {
         System.out.printf("Your number cubed is: %d", cubed);
 
 
-    }
 
+    }
 //    Pod2 Week3 Exercise1 for Chris
 
-    public int countEvens(int[] nums) {
-        int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] % 2 == 0){
-                count++;
-            }
-        }
-        return count;
-    }
 }
