4e61a535d3f70d405cf30bf3554e609d0ef0b73e
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-06 14:13:46 -0600

    chris pod exercises complete

--- src/chrisjava.java
+++ src/chrisjava.java
@@ -31,6 +31,8 @@ public class chrisjava {
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 
         int [] nums = {6, 7, 8, 9, 10};
         System.out.println("removeOdds() = " + returnOdds(nums));
@@ -38,18 +40,17 @@ public class chrisjava {
 
         int [] test = {1, 2, 3, 4, 5};
         System.out.println("countEvens(test) = " + countEvens(test));
-        Scanner scanner = new Scanner(System.in);
 
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Input a Sentence: ");
-        String line = in.nextLine();
-        String upper_case_line = "";
+        String line = input.nextLine();
+        String upperCaseLine = "";
         Scanner lineScan = new Scanner(line);
         while(lineScan.hasNext()) {
             String word = lineScan.next();
-            upper_case_line += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
+            upperCaseLine += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
         }
-        System.out.println(upper_case_line.trim());
+        System.out.println(upperCaseLine.trim());
 
 //        System.out.println("Enter a number you would like to see cubed");
 //        int userNumber = scanner.nextInt();
