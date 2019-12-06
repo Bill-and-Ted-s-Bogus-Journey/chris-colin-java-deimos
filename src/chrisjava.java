1b41be85b16465a46717d8358116dda8c760abab
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-05 21:18:08 -0600

    added two exercises for chrisjava

--- src/chrisjava.java
+++ src/chrisjava.java
@@ -20,4 +20,24 @@ public class chrisjava {
     }
 //    Pod2 Week3 Exercise1 for Chris
 
+    public int countEvens(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String capital(String line) {
+        return Character.toUpperCase(line.charAt(0)) + line.substring(1);
+    }
+
+    public  String reverseString(String string) {
+        String string1 = "Welcome to Beginnersbook";
+        String reversed = reverseString(string1);
+        return "The reversed string is: " + reversed;
+    }
+
 }
